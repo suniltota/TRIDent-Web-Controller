52769c840e27297122ec2583bbf232daf8fae89d
Shravan Kumar Boragala <shravan.boragala@compugain.com>
2017-07-06 20:27:02 +0530

    updated logs

--- src/main/java/com/actualize/mortgage/api/TridentWebAPI.java
+++ src/main/java/com/actualize/mortgage/api/TridentWebAPI.java
@@ -8,6 +8,7 @@ import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,15 +36,14 @@ public class TridentWebAPI {
 	
 	@RequestMapping(value = "/{version}/templatetocdjson", method = { RequestMethod.POST })
 	public ClosingDisclosure templatetocdjson(@PathVariable String version, @RequestBody String txtdoc) throws Exception {
-		LOG.info("Service: CD text template to json object api invoked");
-
+		LOG.info("user "+SecurityContextHolder.getContext().getAuthentication().getName()+" used Service: CD Text Template to CD JSON");
 		return  triDentWebService.convertTemplateToCDJson(txtdoc);	
 	}
 	
 	@RequestMapping(value = "/{version}/templatetolejson", method = { RequestMethod.POST })
 	public LoanEstimate templatetolejson(@PathVariable String version, @RequestBody String txtdoc) throws Exception
 	{
-		LOG.info("Service: LE text template to json object called");
+		LOG.info("user "+SecurityContextHolder.getContext().getAuthentication().getName()+" used Service: LE Text Template to LE JSON");
 		return triDentWebService.convertTemplateToLEJson(txtdoc);
 		
 	}
@@ -51,36 +51,35 @@ public class TridentWebAPI {
 	@RequestMapping(value = "/{version}/calculatecdpayments", method = { RequestMethod.POST })
 	public String calculateCDPayments(@PathVariable String version, @RequestBody ClosingDisclosure closingDisclosure) throws Exception
 	{
-		LOG.info("Service call: cd calculations");
+		LOG.info("user "+SecurityContextHolder.getContext().getAuthentication().getName()+" used Service: CD Calculations");
 		return triDentWebService.calculateCDPayments(closingDisclosure);
 	}
 	
 	@RequestMapping(value = "/{version}/calculatelepayments", method = { RequestMethod.POST })
 	public String calculateLEPayments(@PathVariable String version, @RequestBody LoanEstimate loanEstimateJSON) throws Exception
 	{
-		LOG.info("Service call: le calculations");
+		LOG.info("user "+SecurityContextHolder.getContext().getAuthentication().getName()+" used Service: LE Calculations");
 		return triDentWebService.calculateLEPayments(loanEstimateJSON);
 	}
 	
 	@RequestMapping(value = "/{version}/cdjsontopdf", method = { RequestMethod.POST })
 	public List<PDFResponse> cdJsonToPdf(@PathVariable String version, @RequestBody ClosingDisclosure closingDisclosure) throws Exception
 	{
-		LOG.info("Service: cdjsontopdf called");
-    	
+		LOG.info("user "+SecurityContextHolder.getContext().getAuthentication().getName()+" used Service: CDJSONtoPDF");
 		return triDentWebService.cdJsonToPdf(closingDisclosure);
 	}
 	
 	@RequestMapping(value = "/{version}/lejsontopdf", method = { RequestMethod.POST })
 	public PDFResponse leJsonToPdf(@PathVariable String version, @RequestBody LoanEstimate loanEstimateJSON) throws Exception
 	{
-		LOG.info("Service: lejsontopdf called");
+		LOG.info("user "+SecurityContextHolder.getContext().getAuthentication().getName()+" used Service: LEJSONtoPDF");
     	return triDentWebService.leJsonToPdf(loanEstimateJSON);
 	}
 	
 	@RequestMapping(value = "/{version}/validatecdjson", method = { RequestMethod.POST })
 	public UCDValidationErrors validateCDJson(@PathVariable String version, @RequestBody ClosingDisclosure closingDisclosure) throws Exception
 	{
-		LOG.info("Service: validatecdjson called");
+		LOG.info("user "+SecurityContextHolder.getContext().getAuthentication().getName()+" used Service: ValidateCDJSON");
     	return triDentWebService.validateCDJson(closingDisclosure);
 	}
 	
@@ -94,7 +93,7 @@ public class TridentWebAPI {
 	@RequestMapping(value = "/{version}/cdjsontrim", method = { RequestMethod.POST })
 	public String trimCDJson(@PathVariable String version, @RequestBody ClosingDisclosure closingDisclosure) throws Exception
 	{
-		LOG.info("Service: cdjsontopdf called");
+		LOG.info("user "+SecurityContextHolder.getContext().getAuthentication().getName()+" used Service: CDJSONtoPDF");
     	return triDentWebService.trimCDJson(closingDisclosure);
 	}
 }
