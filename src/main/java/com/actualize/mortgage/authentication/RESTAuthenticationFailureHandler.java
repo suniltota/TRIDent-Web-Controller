7538d8d5166becf144876f4c721a19610389eadc
Mohammed Ali <mohammed.ali@compugain.com>
2017-07-19 17:46:42 +0530

    Sending 403 for invalid credentials

--- src/main/java/com/actualize/mortgage/authentication/RESTAuthenticationFailureHandler.java
+++ src/main/java/com/actualize/mortgage/authentication/RESTAuthenticationFailureHandler.java
@@ -1,11 +1,14 @@
 package com.actualize.mortgage.authentication;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
@@ -13,10 +16,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class RESTAuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
 
+	private static final Logger LOG = LogManager.getLogger(RESTAuthenticationFailureHandler.class);
 	@Override
 	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
 			AuthenticationException exception) throws IOException, ServletException {
-		//super.onAuthenticationFailure(request, response, exception);
-		response.sendError(HttpServletResponse.SC_FORBIDDEN, exception.getMessage());
-	}
+		LOG.error("RESTAuthenticationFailureHandler---------- "+exception.getMessage());
+		response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+
+        PrintWriter writer = response.getWriter();
+        writer.write(exception.getMessage());
+        writer.flush();	
+      }
 }
