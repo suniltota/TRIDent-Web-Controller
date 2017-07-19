23cc8704ba834e57776d817bef20e560f47a4961
Mohammed Ali <mohammed.ali@compugain.com>
2017-07-19 17:46:42 +0530

    Sending 403 for invalid credentials

--- src/main/java/com/actualize/mortgage/authentication/RESTAuthenticationEntryPoint.java
+++ src/main/java/com/actualize/mortgage/authentication/RESTAuthenticationEntryPoint.java
@@ -16,6 +16,6 @@ public class RESTAuthenticationEntryPoint implements AuthenticationEntryPoint {
 	public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
 			throws IOException, ServletException {
 		
-		response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+		response.sendError(HttpServletResponse.SC_UNAUTHORIZED,authException.getMessage());
 	}
 }
